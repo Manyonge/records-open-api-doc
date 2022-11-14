1ab38cad9970474a29cf0a6dbb791255ab163285
Manyonge <77201345+Manyonge@users.noreply.github.com>
2022-11-14 17:54:20 +0300

    all user end points defined

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ProductModule } from './product/product.module';
 import { CustomerModule } from './customer/customer.module';
 import { SaleModule } from './sale/sale.module';
 import { DeliveryModule } from './delivery/delivery.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductModule, CustomerModule, SaleModule, DeliveryModule],
+  imports: [ProductModule, CustomerModule, SaleModule, DeliveryModule, UserModule],
   controllers: [],
   providers: [],
 })
