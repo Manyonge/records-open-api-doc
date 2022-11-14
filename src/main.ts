07acc508d62ecf194b02aa9a6d7f0b9308bb7d05
Manyonge <77201345+Manyonge@users.noreply.github.com>
2022-11-14 19:01:35 +0300

    add rest api server

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = new DocumentBuilder()
     .setTitle('Dripventory records manager')
     .setDescription(
       'An open-api description of the dripventory records manager RESTfulAPI',
     )
+    .addServer('http://localhost:8081')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
